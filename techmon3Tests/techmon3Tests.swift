75b71696dc09db3c7d71afbaa0f6ae39a00fd1c8
O-suke12 <souke0504@gmail.com>
2020-05-14 22:26:05 +0900

    Initial Commit

---
+++ techmon3Tests/techmon3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  techmon3Tests.swift
+//  techmon3Tests
+//
+//  Created by RS on 2020/05/14.
+//  Copyright © 2020 osuke. All rights reserved.
+//
+
+import XCTest
+@testable import techmon3
+
+class techmon3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
